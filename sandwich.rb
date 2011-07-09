ca3876f9ebc6d3489ae7aff142289fc9d8abc224
Sebastian Boehm <sebastian@sometimesfood.org>
2011-07-09 03:10:55 -0700

    Install some package

--- sandwich.rb
+++ sandwich.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require 'chef/run_context'
 require 'chef/client'
+require 'chef/recipe'
+require 'chef/resource'
+require 'chef/providers'
 
 $client = nil
 
@@ -15,9 +18,16 @@ def run_chef
   rebuild_node
   Chef::Log.level = :debug
   run_context = Chef::RunContext.new($client.node, {})
+#  @recipe = Chef::Recipe.new(nil, nil, run_context)
   runrun = Chef::Runner.new(run_context).converge
-  Chef::Log.level = :info
+  Chef::Log.level = :debug
   runrun
+  install_package('gitg', run_context)
+end
+
+def install_package(package_name, run_context)
+  package = Chef::Resource::Package.new(package_name, run_context)
+  package.run_action(:install)
 end
 
 puts 'Hello chef run context world'
