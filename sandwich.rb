086eaac162741b9c91d26816356e3a4c3eea2073
Sebastian Boehm <sebastian@sometimesfood.org>
2011-07-10 05:04:07 -0700

    Remove cruft

--- sandwich.rb
+++ sandwich.rb
@@ -1,9 +1,5 @@
 #!/usr/bin/env ruby
-require 'chef/run_context'
-require 'chef/client'
-require 'chef/recipe'
-require 'chef/resource'
-require 'chef/providers'
+require 'chef'
 
 class Chef::Recipe
   def from_string(string)
@@ -32,15 +28,4 @@ def run_chef
   runrun
 end
 
-def install_package(package_name, run_context)
-  package = Chef::Resource::Package.new(package_name, run_context)
-  package.run_action(:install)
-end
-
-def add_package_to_run_context(package_name, run_context)
-  package = Chef::Resource::Package.new(package_name, run_context)
-  run_context.resource_collection.insert(package)
-end
-
-puts 'Hello chef run context world'
 run_chef
