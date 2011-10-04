50f0fa7cfb6e092849accdef6bb363fb39461827
Sebastian Boehm <sebastian@sometimesfood.org>
2011-10-04 19:51:57 +0200

    Suppress output for empty run list

--- lib/sandwich/client.rb
+++ lib/sandwich/client.rb
@@ -12,6 +12,11 @@ module Sandwich
       @sandwich_filename = filename
     end
 
+    def build_node
+      # silence build_node from super class
+      silence_chef { super }
+    end
+
     def setup_run_context
       run_context = super
       cookbook = Sandwich::CookbookVersion.new(@sandwich_cookbook)
@@ -19,6 +24,15 @@ module Sandwich
       recipe = Chef::Recipe.new(@sandwich_cookbook, nil, run_context)
       recipe.from_string(@sandwich_recipe, @sandwich_filename)
       run_context
+    end
+
+    private
+    def silence_chef
+      log_level = Chef::Log.level
+      Chef::Log.level = :fatal
+      yield
+    ensure
+      Chef::Log.level = log_level
     end
   end
 end
