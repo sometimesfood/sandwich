c9f96867bbb25356809f6d56492162554f03d31a
Sebastian Boehm <sebastian@sometimesfood.org>
2015-01-19 13:22:21 +0100

    Update spec_helper for Chef 11

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,13 +22,6 @@ def ohai_data(key)
   ohai.data[key]
 end
 
-# monkey patch for https://github.com/defunkt/fakefs/issues/96
-class FakeFS::Dir
-  def self.mkdir(path, integer = 0)
-    FileUtils.mkdir(path)
-  end
-end
-
 def runner_from_recipe(recipe)
   Sandwich::Runner.new('<SPEC_HELPER>', recipe)
 end
@@ -37,28 +30,46 @@ def run_recipe(recipe)
   runner_from_recipe(recipe).run(:fatal)
 end
 
-# fakefs does not support IO.read (which is used a lot by chef),
-# therefore monkey patch IO.read to use FakeFS::File.read
-def with_fake_io_read
+# fakefs does not support IO.binread (which is used a lot by chef),
+# therefore monkey patch IO.binread to use FakeFS::File.binread
+def with_fake_io_binread
   IO.instance_eval do
-    alias :read_orig :read
-    def self.read(path); FakeFS::File.read(path); end
+    alias :binread_orig :binread
+    def self.binread(path); FakeFS::File.binread(path); end
   end
   yield
 ensure
-  IO.instance_eval { alias :read :read_orig }
+  IO.instance_eval { alias :binread :binread_orig }
 end
 
 def with_fakefs
   FakeFS do
     setup_standard_dirs
-    with_fake_io_read { yield }
+    with_fake_io_binread { yield }
   end
 end
 
 def setup_standard_dirs
   FileUtils.mkdir_p '/tmp'
+  FileUtils.mkdir_p Chef::Config[:file_cache_path]
 end
 
 # make sure Chef 0.10 exceptions are available when using older Chef versions
 class Chef::Exceptions::EnclosingDirectoryDoesNotExist; end
+
+Chef::Config[:client_fork] = false
+Chef::Config[:force_logger] = true
+
+# FakeFS does not provide File.realpath, but for our simple tests
+# expand_path is close enough
+class FakeFS::File
+  def self.realpath(*args)
+    expand_path(*args)
+  end
+end
+
+# disable Chef run locks
+class Chef::RunLock
+  def test; true; end
+  def save_pid; end
+end
